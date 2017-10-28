963dcc2c89efd846e05e59aabe2c2056581543c9
Alberto Marley <berbus8@gmail.com>
2017-10-28 03:25:38 +0200

    layout lista

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ DEBUG = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', nombre = nombre)
+	return render_template('index.html')
 
 
 if __name__ == '__main__':
