f3f4025533bab374ead8409fab1baa3c389f7858
Alberto Marley <berbus8@gmail.com>
2017-11-01 18:06:09 +0100

    persistencia con sqlite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os, shelve
+import sqlite3 as sq3
 from flask import Flask, request, render_template, session, redirect
 
 app = Flask(__name__)
@@ -9,7 +10,9 @@ DEBUG = True
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		db = shelve.open('usuarios.db')
+		conn = sq3.connect('datos.db')
+		db = conn.cursor()
+
 		session['msg'] = ""
 
 		try:
@@ -21,24 +24,28 @@ def index():
 			pwd = 'error'
 
 		if 'entrar' in request.form:
-			if (usr in db) and (db[usr] == pwd):
+			query = "SELECT user FROM usuarios WHERE user = '" + usr + "' and pass = '" + pwd + "'"
+			db.execute(query)
+
+			if db.fetchone():
 				session['logged_in'] = True
 				session['usr'] = usr
 			else:
 				session['msg'] = "Los datos introducidos no se corresponden con los de ningún usuario registrado"
+
 		elif 'registrar' in request.form:
-			if (usr not in db):
-				db['usr'] = pwd
+			query = "SELECT user FROM usuarios WHERE user = '" + usr + "'"
+			db.execute(query)
+			print(query)
+
+			if not db.fetchone():
+				query = "INSERT INTO usuarios ('user','pass') VALUES ('" + usr + "','" + pwd + "')"
+				print(query)
 				session['msg'] = "¡Usuario creado con éxito! Ya puedes acceder..."
 			else:
 				session['msg'] = "Ya existe un usuario con ese nombre"
-
-		db.close()
-
-	if not session.get('logged_in'):
-		return render_template('index.html')
-	else:
-		return render_template('index.html')
+		conn.close()
+	return render_template('index.html')
 
 @app.route('/logout')
 def logout():
