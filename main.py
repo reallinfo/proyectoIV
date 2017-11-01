8c31f0b003d8b85426ca2f872ac61b0e640bc042
Alberto Marley <berbus8@gmail.com>
2017-11-01 17:38:34 +0100

    persistencia con shelve mas o menos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, shelve
 from flask import Flask, request, render_template, session, redirect
 
 app = Flask(__name__)
@@ -9,17 +9,31 @@ DEBUG = True
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		if request.form['pwd'] == 'pass' and request.form['usr'] == 'user':
-			session['logged_in'] = True
-		print("POST")
-	else:
-		print("GET")
+		db = shelve.open('usuarios.db')
+		session['msg'] = ""
 
-	if 'entrar' in request.form:
-		print("Entrar")
-	elif 'registrar' in request.form:
-		print("Registrar")
-	'''print(request.form['registrar'])'''
+		try:
+			usr = str(request.form['usr'])
+			pwd = str(request.form['pwd'])
+		except:
+			session['msg'] = "Los datos introducidos no son válidos"
+			usr = 'error'
+			pwd = 'error'
+
+		if 'entrar' in request.form:
+			if (usr in db) and (db[usr] == pwd):
+				session['logged_in'] = True
+				session['usr'] = usr
+			else:
+				session['msg'] = "Los datos introducidos no se corresponden con los de ningún usuario registrado"
+		elif 'registrar' in request.form:
+			if (usr not in db):
+				db['usr'] = pwd
+				session['msg'] = "¡Usuario creado con éxito! Ya puedes acceder..."
+			else:
+				session['msg'] = "Ya existe un usuario con ese nombre"
+
+		db.close()
 
 	if not session.get('logged_in'):
 		return render_template('index.html')
@@ -29,6 +43,8 @@ def index():
 @app.route('/logout')
 def logout():
 	session.pop('logged_in')
+	session.pop('msg')
+	session.pop('usr')
 	return redirect('/')
 
 
