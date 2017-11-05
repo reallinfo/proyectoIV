1bd5da1156342416b76dab5b83c84378d5118bdb
berbus <berbus8@gmail.com>
2017-11-05 21:59:38 +0100

    al final la culpa siempre es de otro

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from urllib import parse
 from flask import Flask, request, render_template, session, redirect
 
 app = Flask(__name__)
-app.secret_key = os.urandom(12)
+app.secret_key = "cosabrutalmentealeatoriacuidado"
 
 PORT = 8000
 DEBUG = True
@@ -12,52 +12,48 @@ DEBUG = True
 ''' Cuando acabe con las pruebas cambiaré la contraseña y quitaré esto. '''
 MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://prueba:123456@ds245805.mlab.com:45805/base')
 
-print(MONGO_URL)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	print("si pasa")
-	if request.method == 'POST':
-
-		if not session.get('logged_in'):
-			client = pymongo.MongoClient(MONGO_URL)
-			col = client.base.users_iv
-			session['msg'] = ""
+	if request.method == 'POST' and not session.get('logged_in'):
+		client = pymongo.MongoClient(MONGO_URL)
+		col = client.base.users_iv
+		session['msg'] = ""
+		res = col.find()
+		try:
+			usr = str(request.form['usr'])
+			pwd = str(request.form['pwd'])
 			res = col.find()
-			try:
-				usr = str(request.form['usr'])
-				pwd = str(request.form['pwd'])
-				res = col.find()
-			except:
-				session['msg'] = "Datos no válidos"
-				usr = 'error'
-				pwd = 'error'
+		except:
+			session['msg'] = "Datos no válidos"
+			usr = 'error'
+			pwd = 'error'
 
-			if ('entrar' in request.form) and (usr != 'error'):
+		if ('entrar' in request.form) and (usr != 'error'):
 
-				for i in res:
-					aux = i
-					if (aux['user'] == usr) and (aux['pass'] == pwd):
-						session['logged_in'] = True
-						session['usr'] = usr
+			for i in res:
+				aux = i
+				if (aux['user'] == usr) and (aux['pass'] == pwd):
+					session['logged_in'] = True
+					session['usr'] = usr
 
-				if not session.get('logged_in'):
-					session['msg'] = "Datos incorrectos"
+			if not session.get('logged_in'):
+				session['msg'] = "Datos incorrectos"
 
-			elif ('registrar' in request.form) and (usr != 'error'):
-				existe = False
-				for i in res:
-					aux = i
-					if aux['user'] == usr:
-						existe = True
+		elif ('registrar' in request.form) and (usr != 'error'):
+			existe = False
+			for i in res:
+				aux = i
+				if aux['user'] == usr:
+					existe = True
 
-				if existe:
-					session['msg'] = "Ya existe un usuario con ese nombre"
-				else:
-					col.insert_one( {'user':usr, 'pass':pwd} )
-					session['msg'] = "¡Usuario creado con éxito!"
-			client.close()
-			
+			if existe:
+				session['msg'] = "Ya existe un usuario con ese nombre"
+			else:
+				col.insert_one( {'user':usr, 'pass':pwd} )
+				session['msg'] = "¡Usuario creado con éxito!"
+		client.close()
+
 	return render_template('index.html')
 
 
@@ -68,7 +64,7 @@ def logout():
 	session.pop('usr', None)
 	return redirect('/')
 
-'''
+
 @app.route('/cambiopass', methods = ['POST'])
 def cambiopass():
 	client = pymongo.MongoClient(MONGO_URL)
@@ -104,7 +100,6 @@ def cambiopass():
 				session['msg'] = "Contraseña actual inválida"
 
 	return redirect('/')
-'''
 
 
 if __name__ == '__main__':
