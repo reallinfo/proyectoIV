06b2e6ca8a04c737709b9ef7d15b3e1b48a53418
berbus <berbus8@gmail.com>
2018-09-22 19:59:05 +0200

    cambiada url para el deploy

--- static/js/index-controller.js
+++ static/js/index-controller.js
@@ -4,7 +4,7 @@ app.controller("indexController", ["$scope", "$http", function($scope, $http) {
     $scope.random_n = 0;
     $scope.min_n = 0;
     $scope.max_n = 100000;
-    var addr = "localhost:8000";
+    var addr = "https://aleatoriedapp.herokuapp.com";
 
     $scope.getRandomN = function() {
         $scope.random_n = 0;
@@ -15,7 +15,7 @@ app.controller("indexController", ["$scope", "$http", function($scope, $http) {
 
         $http({
             method: "GET",
-            url: "http://" + addr + "/random/random_number",
+            url: addr + "/random/random_number",
             params: params
         }).then(function(response) {
             $scope.random_n = response.data.random_number;
--- static/js/random.js
+++
@@ -1,5 +0,0 @@
-
-
-app.service("randomSrvc", function(){
-    
-})
