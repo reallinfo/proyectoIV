13ed58c3cb6167d32497c1537305ce4abe6dbb54
berbus <berbus8@gmail.com>
2018-10-06 13:27:40 +0200

    añadidos mensajes a los tests

--- aleatoriedad/test/test_views.py
+++ aleatoriedad/test/test_views.py
@@ -5,8 +5,10 @@ import json
 
 class AleatoriedadViewsTest(TestCase):
     def test_random_number(self):
+        print('Probando La vista de números aleatorios')
+        print('Probando resultado con bad input')
         res = self.client.get('/random/random_number')
-        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.status_code, 400, 'El código devuelto por el servidor no es el esperado')
 
         res = self.client.get('/random/random_number?max=0&min=0')
-        self.assertEqual(res.json()['random_number'], 0)
+        self.assertEqual(res.json()['random_number'], 0, 'El valor devuelto no es el esperado.')
